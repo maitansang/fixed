7df5d4aff92be6c9502cf5a3f1f33063640acc78
maitansang <maitansang200599@gmail.com>
2021-09-22 20:54:52 +0700

    Add struct

--- list_ticker/go.mod
+++ list_ticker/go.mod
@@ -1,3 +1,8 @@
 module list_ticker
 
 go 1.15
+
+require (
+	gorm.io/driver/postgres v1.1.1 // indirect
+	gorm.io/gorm v1.21.15 // indirect
+)
--- list_ticker/utils/mainFile.go
+++ list_ticker/utils/mainFile.go
@@ -1,19 +1,19 @@
 package utils
 
 import (
-	"fmt"
 	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
 type DB struct {
 	*gorm.DB
 }
 
 func InitDB() (*DB, error) {
 	// handle db
-	dsn := "host=52.116.150.66 user=dev_user password=Dev$54321 dbname=transaction_db port=5433 sslmode=disable"
+	dsn := "host=52.116.150.66 user=postgres password=P`AgD!9g!%~hz3M< dbname=stockmarket port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Println("can not open db")
@@ -23,10 +23,31 @@ func InitDB() (*DB, error) {
 	}
 	return DB, nil
 }
+
+type Ticker struct {
+	ID     string `gorm:"primaryKey;autoIncrement:false"`
+	Symbol string `json:"symbol" `
+}
+type Dailybar struct {
+	ID     string `gorm:"primaryKey;autoIncrement:false"`
+	Ticker string `json:"ticker" `
+	V      string `json:"v"`
+}
+
 func MainFunc() {
 	db, err := InitDB()
-	if err !=nil {
+	if err != nil {
 		log.Println("can not init db", err)
 	}
-	fmt.Println("Ok")
+
+	tickers, err := condition1(db)
+	log.Println("=====", tickers, err)
+}
+
+// 1 Ticker name must be there in dailybars, largest_orders and short_interest and in each table it's row count must be 700
+func condition1(db *DB) ([]Ticker, error) {
+	log.Println("----")
+	var tickers []Ticker
+	db.Distinct("symbol").Order("symbol desc").Find(&tickers)
+	return tickers, nil
 }
