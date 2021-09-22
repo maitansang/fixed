cee3dcc9a39adf35204c5e1ff2f66254f96950d1
maitansang <maitansang200599@gmail.com>
2021-09-22 20:18:27 +0700

    Init DB

--- list_ticker/utils/mainFile.go
+++ list_ticker/utils/mainFile.go
@@ -2,9 +2,31 @@ package utils
 
 import (
 	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+type DB struct {
+	*gorm.DB
+}
 
+func InitDB() (*DB, error) {
+	// handle db
+	dsn := "host=52.116.150.66 user=dev_user password=Dev$54321 dbname=transaction_db port=5433 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("can not open db")
+	}
+	DB := &DB{
+		db,
+	}
+	return DB, nil
+}
 func MainFunc() {
-
+	db, err := InitDB()
+	if err !=nil {
+		log.Println("can not init db", err)
+	}
 	fmt.Println("Ok")
 }
