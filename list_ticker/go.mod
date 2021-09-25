b6605fee6e41bce939194b76eb3d8f20351dadc8
maitansang <maitansang200599@gmail.com>
2021-09-25 23:40:17 +0700

    Increase speed execute

--- list_ticker/go.mod
+++ list_ticker/go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/gammazero/workerpool v1.1.2
-	gorm.io/driver/postgres v1.1.1 // indirect
-	gorm.io/gorm v1.21.15 // indirect
+	gorm.io/driver/postgres v1.1.1
+	gorm.io/gorm v1.21.15
 )
--- list_ticker/utils/mainFile.go
+++ list_ticker/utils/mainFile.go
@@ -68,163 +68,71 @@ func MainFunc() {
 	conditionString := os.Args[1]
 	conditions = strings.Split(conditionString, ",")
 	fmt.Println(!isValid("6"))
-
-	tickers, err := db.getAllTicker()
+	allTickers, err := db.getAllTicker()
 	if err != nil {
 		log.Println("Error when get all ticker", err)
 	}
 
-	wpool := workerpool.New(400)
-	var newTickers []string
-	for _, ticker := range tickers {
-		// index := i
-		ticker := ticker
+	tickersCondition1, err := db.condition1()
+	if err != nil {
+		log.Fatal("Err condition 1 ", err)
+	}
+	fmt.Println("len 1", len(tickersCondition1))
+
+	tickersCondition2, err := db.condition2()
+	if err != nil {
+		log.Fatal("Err condition 2 ", err)
+	}
+	fmt.Println("len 2", len(tickersCondition2))
+
+	tickersCondition3, err := db.condition3()
+	if err != nil {
+		log.Fatal("Err condition 3 ", err)
+	}
+	fmt.Println("len 3", len(tickersCondition3))
+
+	tickersCondition4, err := db.condition4()
+	if err != nil {
+		log.Fatal("Err condition 4 ", err)
+	}
+	fmt.Println("len 4", len(tickersCondition4))
+
+	tickersCondition5, err := db.condition5()
+	if err != nil {
+		log.Fatal("Err condition 5 ", err)
+	}
+	fmt.Println("len 5", len(tickersCondition5))
+
+	tickersCondition6, err := db.condition6()
+	if err != nil {
+		log.Fatal("Err condition 6 ", err)
+	}
+	fmt.Println("len 6", len(tickersCondition6))
+
+	wpool := workerpool.New(3000)
+	var resultTickers []string
+	fmt.Println("is valid 1", !isValid("1"))
+	isInvalid1 := !isValid("1")
+	isInvalid2 := !isValid("2")
+	isInvalid3 := !isValid("3")
+	isInvalid4 := !isValid("4")
+	isInvalid5 := !isValid("5")
+	isInvalid6 := !isValid("6")
+
+	for _, item := range allTickers {
+		item := item
 		wpool.Submit(func() {
-			if db.condition1(ticker) && db.condition2(ticker) && db.condition3(ticker) && db.condition4(ticker) && db.condition5(ticker) && condition6(ticker) {
-				newTickers = append(newTickers, ticker)
+			if (contains(tickersCondition1, item) || isInvalid1) && (contains(tickersCondition2, item) || isInvalid2) && (contains(tickersCondition3, item) || isInvalid3) && (contains(tickersCondition4, item) || isInvalid4) && (contains(tickersCondition5, item) || isInvalid5) && (contains(tickersCondition6, item) || isInvalid6) {
+				resultTickers = append(resultTickers, item)
 			}
 		})
 	}
+
 	wpool.StopWait()
-	writeFile(newTickers)
-}
+	writeFile(resultTickers)
+	fmt.Println("Result", resultTickers)
+	fmt.Println("Len result", len(resultTickers))
 
-func isValid(condition string) bool {
-	for _, c := range conditions {
-		if c == condition {
-			return true
-		}
-	}
-	return false
-}
-
-// 1 Ticker name must be there in dailybars, largest_orders and short_interest and in each table it's row count must be 700
-func (db *DB) condition1(ticker string) bool {
-	if !isValid("1") {
-		return true
-	}
-	var count1, count2, count3 int64
-
-	err := db.DB.Table("dailybars").
-		Select("count(*)").
-		Where("ticker = ?", ticker).
-		Count(&count1).
-		Error
-	if err != nil {
-		log.Fatalln("Error when find ticker has change greater than 700", err)
-		return false
-	}
-
-	err = db.DB.Table("largestorders").
-		Select("count(*)").
-		Where("ticker = ?", ticker).
-		Count(&count2).
-		Error
-	if err != nil {
-		log.Fatalln("Error when find ticker has change greater than 700", err)
-		return false
-	}
-
-	err = db.DB.Table("short_interest").
-		Select("count(*)").
-		Where("ticker = ?", ticker).
-		Count(&count3).
-		Error
-	if err != nil {
-		log.Fatalln("Error when find ticker has change greater than 700", err)
-		return false
-	}
-
-	return count1 >= 700 && count2 >= 700 && count3 >= 700
-}
-
-// 2 Ticker must have lastest date closing price below 10
-func (db *DB) condition2(ticker string) bool {
-	if !isValid("2") {
-		return true
-	}
-	var closingPrice float64
-	err := db.DB.Raw("SELECT c FROM dailybars WHERE DATE=(SELECT MAX(DATE) FROM dailybars) AND ticker='AAPL'").Scan(&closingPrice).Error
-	if err != nil {
-		log.Println("Error when ticket has lastest date closing price below 10 ", err)
-		return false
-	}
-	return closingPrice < 10
-}
-
-// 3 Ticker must have latest date volume greater than 50,000, volume is "v" in dailybars
-func (db *DB) condition3(ticker string) bool {
-	if !isValid("3") {
-		return true
-	}
-	var count int64
-	err := db.DB.Table("dailybars_duplicate").
-		Select("count(*)").
-		Where("v>5000 AND ticker = ?", ticker).Count(&count).Error
-	if err != nil {
-		log.Println("Error when count ticket has volume greater than 50000 ", err)
-		return false
-	}
-	return count >= 1
-}
-
-// 4 Ticker must have at least 10 rows where it's change3 value is greater than 30 (dailybars_duplicate)
-func (db *DB) condition4(ticker string) bool {
-	if !isValid("4") {
-		return true
-	}
-	var count int64
-	err := db.DB.Table("dailybars_duplicate").
-		Select("count(*)").
-		Where("change3>30 AND ticker = ?", ticker).Count(&count).Error
-	if err != nil {
-		log.Println("Error ticker must have at least 10 rows where it's change3 value is greater than 30 ", err)
-		return false
-	}
-	return count >= 10
-}
-
-// 5 Ticker must have atleast 100 rows where it's change value is either greater than 3 or below -3
-func (db *DB) condition5(ticker string) bool {
-	if !isValid("5") {
-		return true
-	}
-	var count int64
-	err := db.DB.Table("dailybars").
-		Select("count(*)").
-		Where("(change > 3 OR change < 3) AND ticker = ?", ticker).
-		Count(&count).
-		Error
-	if err != nil {
-		log.Println("Error when find ticker has change greater than 100", err)
-		return false
-	}
-	return count >= 100
-}
-
-// 6 Ticker name must also be there in input_ticker.txt file
-func condition6(ticker string) bool {
-	if !isValid("6") {
-		return true
-	}
-	file, err := os.Open("input_ticker.txt")
-	if err != nil {
-		log.Println("Error when get ticker from text file", err.Error())
-		return false
-	}
-	defer file.Close()
-
-	var inputTickers []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		inputTickers = append(inputTickers, scanner.Text())
-	}
-	for _, t := range inputTickers {
-		if t == ticker {
-			return true
-		}
-	}
-	return false
 }
 
 func writeFile(tickers []string) error {
@@ -239,4 +147,153 @@ func writeFile(tickers []string) error {
 		fmt.Fprintln(w, ticker)
 	}
 	return w.Flush()
+}
+
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
+
+func isValid(condition string) bool {
+	for _, c := range conditions {
+		if c == condition {
+			return true
+		}
+	}
+	return false
+}
+
+// 1 Ticker name must be there in dailybars, largest_orders and short_interest and in each table it's row count must be 700
+func (db *DB) condition1() ([]string, error) {
+	var newTickers []string
+	if !isValid("1") {
+		return newTickers, nil
+	}
+	var newTickersDailybars []string
+	err := db.DB.Raw("select ticker from dailybars where ticker is not null group by ticker having count(id) > 700").Scan(&newTickersDailybars).Error
+	if err != nil {
+		log.Fatal("Error when find ticker has change greater than 700", err)
+		return newTickers, err
+	}
+
+	var newTickersLargestorders []string
+	err = db.DB.Raw("select ticker from largestorders where ticker is not null group by ticker having count(id) > 700").Scan(&newTickersLargestorders).Error
+	if err != nil {
+		log.Fatal("Error when find ticker has change greater than 700", err)
+		return newTickers, err
+	}
+
+	var newTickersShortInterest []string
+	err = db.DB.Raw("select ticker  from short_interest where ticker is not null group by ticker having count(id) > 700").Scan(&newTickersShortInterest).Error
+	if err != nil {
+		log.Fatal("Error when find ticker has change greater than 700", err)
+		return newTickers, err
+	}
+	for _, v := range newTickersDailybars {
+		if contains(newTickersLargestorders, v) && contains(newTickersShortInterest, v) {
+			newTickers = append(newTickers, v)
+		}
+	}
+	return newTickers, nil
+}
+
+// 2 Ticker must have lastest date closing price below 10
+func (db *DB) condition2() ([]string, error) {
+	var tickers []string
+
+	if !isValid("2") {
+		return tickers, nil
+	}
+	err := db.DB.Raw("SELECT ticker FROM dailybars WHERE DATE=(SELECT MAX(DATE) FROM dailybars) AND c < 10").Scan(&tickers).Error
+	if err != nil {
+		log.Println("Error when ticket has lastest date closing price below 10 ", err)
+		return tickers, err
+	}
+	return tickers, err
+}
+
+// 3 Ticker must have latest date volume greater than 50,000, volume is "v" in dailybars
+func (db *DB) condition3() ([]string, error) {
+	var tickers []string
+
+	if !isValid("3") {
+		return tickers, fmt.Errorf("Invalid condition")
+	}
+
+	var maxDate string
+
+	err := db.DB.Table("dailybars").
+		Select("max(date)").Scan(&maxDate).Error
+	if err != nil {
+		log.Println("Error when count ticket has volume greater than 50000 ", err)
+		return tickers, err
+	}
+
+	err = db.DB.Table("dailybars").
+		Select("ticker").
+		Where("v>5000 AND date= ?", maxDate).Scan(&tickers).Error
+	if err != nil {
+		log.Println("Error when count ticket has volume greater than 50000 ", err)
+		return tickers, err
+	}
+
+	return tickers, err
+}
+
+// 4 Ticker must have at least 10 rows where it's change3 value is greater than 30 (dailybars_duplicate)
+func (db *DB) condition4() ([]string, error) {
+	var tickers []string
+	if !isValid("4") {
+		return tickers, fmt.Errorf("Invalid condition")
+	}
+
+	err := db.DB.Raw("Select ticker from dailybars_duplicate where change3>30 group by ticker having count(ticker)>=10").Scan(&tickers).Error
+	if err != nil {
+		log.Println("Error ticker must have at least 10 rows where it's change3 value is greater than 30 ", err)
+		return tickers, err
+	}
+	return tickers, err
+}
+
+// 5 Ticker must have atleast 100 rows where it's change value is either greater than 3 or below -3
+func (db *DB) condition5() ([]string, error) {
+	var tickers []string
+
+	if !isValid("5") {
+		return tickers, nil
+	}
+	err := db.DB.Table("dailybars").
+		Select("ticker").
+		Where("(change > 3 OR change < 3)").Group("ticker").Having("COUNT(*) > 99 ").Scan(&tickers).
+		Error
+	if err != nil {
+		log.Println("Error when find ticker has change greater than 100", err)
+		return tickers, err
+	}
+	return tickers, err
+}
+
+// 6 Ticker name must also be there in input_ticker.txt file
+func (db *DB) condition6() ([]string, error) {
+	var tickers []string
+	if !isValid("6") {
+		return tickers, nil
+	}
+	file, err := os.Open("input_ticker.txt")
+	if err != nil {
+		log.Println("Error when get ticker from text file", err.Error())
+		return tickers, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		tickers = append(tickers, scanner.Text())
+	}
+
+	return tickers, err
 }
