c5e4224878aa8f3f2ebc455f06890efeb37166ab
maitansang <maitansang200599@gmail.com>
2021-11-06 17:13:27 +0700

    Update done

--- short_sale/utils/mainFile.go
+++ short_sale/utils/mainFile.go
@@ -244,16 +244,11 @@ func insertData(db *DB, arr []ShortSale, date string) error {
 	calLoop := math.Ceil(loop)
 	intLoop := int(calLoop)
 	var listStartEndPoint = make(map[int64]int64)
-	log.Println("listStartEndPoint", listStartEndPoint)
 	for i := 0; i <= int(calLoop); i += 1 {
 		start := (len(arr) / intLoop) * i
 		end := (len(arr) / intLoop) * (i + 1)
 		if (i + 1) >= intLoop {
-			log.Println("=================i", i)
-			log.Println("=================start", start)
-			log.Println("=================end", end)
-			log.Println("=================len(arr)", len(arr))
-			// end = len(arr)
+			end = len(arr)
 		}
 		listStartEndPoint[int64(start)] = int64(end)
 		// existTable := db.Migrator().HasTable("short_sale_" + dateTable)
@@ -279,8 +274,6 @@ func insertData(db *DB, arr []ShortSale, date string) error {
 		// log.Println("start of end ", start, end)
 		// log.Println("value of i ", i)
 	}
-	log.Println("=================int(calLoop)", int(calLoop))
-	log.Fatal("=================intLoop", intLoop)
 	for i := 100; i <= len(listStartEndPoint); i += 100 {
 		log.Println("================ workerpool i", i)
 		insertDatabase := workerpool.New(i)
