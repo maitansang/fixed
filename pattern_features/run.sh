5299f73c7c26672083b2178298d5259bd7e1d7ca
Sang <maitansang.200599@gmail.com>
2021-11-16 23:09:33 +0700

    Dit me tu nhien mat file run.sh

---
+++ pattern_features/run.sh
@@ -0,0 +1,2 @@
+export PGPASSWORD='P`AgD!9g!%~hz3M<'
+psql -h 52.116.150.66 -d stockmarket -U postgres -c "\copy pattern_features (ticker,date,co,value20_days_change_pct,above200_ma)  from 'data.csv' with delimiter as ','"
