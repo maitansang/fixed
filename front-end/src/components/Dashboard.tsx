77eec4e1d59f2de8fc439b87cc65313c98be8e90
maitansang <maitansang200599@gmail.com>
2021-11-17 19:33:04 +0700

    Update

--- front-end/src/components/Dashboard.tsx
+++ front-end/src/components/Dashboard.tsx
@@ -6,84 +6,83 @@ const Dashboard: React.FC = () => {
   const currentUser = getCurrentUser();
   const [runScript, setRunScript] = useState('');
 
-  function buttonHandler(text : any) : any {
+  const buttonHandler = (text : any) => {
     setRunScript(text);
   };
-
   return (
     <div className="container">
       <div className="right-menu-container">
-        <div className="mm-item" onClick={buttonHandler('dashboard')}>
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")} >
           <a href="">
             <span className="">Dashboard</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Aggreagates</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Average Volume</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Trades</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Breakouthis</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Changepct</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Changepctall</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Lob</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Lov</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Pattern Features</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}  >
           <a href="">
             <span className="">Short</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Shot Sale</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Stock Split</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Tickers</span>
           </a>
         </div>
-        <div className="mm-item" >
+        <div className="mm-item" onClick={() => buttonHandler("dashboard")}>
           <a href="">
             <span className="">Transactions</span>
           </a>
