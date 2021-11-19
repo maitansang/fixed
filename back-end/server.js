e59ba0edad891537eaadb3a6512bd1d80583d558
maitansang <maitansang200599@gmail.com>
2021-11-19 19:16:00 +0700

    Open cors

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "*"
 };
 
 app.use(cors(corsOptions));
